d10b4857d078425002c9050d887d51ae7c5baab4
MrNewaz <saifur.saif@northsouth.edu>
2021-06-25 00:35:56 +0600

    12 done

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,16 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-transformer-remark",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+  ],
   siteMetadata: {
     title: "Web Warrior",
     description: "Web Dev Portfolio",
